9205ca3cf1a5a3d243994f34d909148c4b068a62
uto-usui <me@uto-usui.com>
2019-11-18 14:24:23 +0900

    🚧 Work in progress - TypeScript settings

---
+++ docs/.vuepress/components/TypeScriptTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <section ref="wrap" class="wrap">
+    djhfjhasdhfahshdkjsa
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type TypeScriptTestData = {
+  text: string
+}
+
+export default Vue.extend({
+  name: 'TypeScriptTest',
+  data: (): TypeScriptTestData => ({
+    text: 'test'
+  }),
+  mounted(): void {
+    //
+  },
+})
+</script>
+
+<style scoped lang="scss">
+//
+</style>
