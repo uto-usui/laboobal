fcf7681134516abd3ea478f504de51fb54cf2ccf
uto-usui <me@uto-usui.com>
2019-11-19 12:31:59 +0900

    ✨ Add gsap Stagger

---
+++ docs/.vuepress/components/basic/gsap/Stagger.vue
@@ -0,0 +1,75 @@
+<template>
+  <section id="js-wrap" ref="wrap" class="wrap">
+    <div class="inner">
+      <div v-for="index in 100" :key="`target${index}`" ref="target" class="target"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { gsap } from 'gsap/all'
+
+export default {
+  name: 'Basic',
+  components: {},
+  data() {
+    return {
+      //
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.animation();
+    })
+  },
+  destroyed() {
+    //
+  },
+  methods: {
+    animation() {
+      const { target } = this.$refs
+
+      gsap.to(target, {
+        duration: 1.25,
+        scale: 0.75,
+        // rotationY: 360,
+        z: 100,
+        repeat: -1,
+        repeatDelay: 0.2,
+        yoyo: true,
+        ease: 'Sine.inOut',
+        stagger: {
+          amount: 1,
+          grid: [10, 10],
+          from: 'center',
+        },
+      })
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.wrap {
+  position: relative;
+  z-index: 9;
+  background-color: #f1f1f1;
+  perspective: 800px;
+}
+
+.inner {
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  height: 500px;
+  transform: translateZ(-800px) rotateX(60deg) rotateY(30deg);
+  transform-style: preserve-3d;
+  perspective: 800px;
+}
+
+.target {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #25ecb7;
+}
+</style>
